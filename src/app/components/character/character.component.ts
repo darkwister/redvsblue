cecbbeb4c6c6c061e7241863424952aa7aaf8a74
DarkWister <darkwistercrak17@gmail.com>
2025-02-26 16:51:01 -0400

    added the common module to components

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.scss'],
+  imports: [CommonModule],
+  standalone: true
 })
 export class CharacterComponent  implements OnInit {
 
--- src/app/components/moments/moments.component.ts
+++ src/app/components/moments/moments.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moments',
   templateUrl: './moments.component.html',
   styleUrls: ['./moments.component.scss'],
+  imports: [ CommonModule ],
+  standalone: true
 })
 export class MomentsComponent  implements OnInit {
 
