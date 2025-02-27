d11cccfc6ad7735fedfb928ca65c7b022f348bd8
DarkWister <darkwistercrak17@gmail.com>
2025-02-27 18:51:25 -0400

    hire me page finished

--- src/app/pages/hire-me/hire-me.page.ts
+++ src/app/pages/hire-me/hire-me.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-hire-me',
   templateUrl: './hire-me.page.html',
   styleUrls: ['./hire-me.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,  IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg]
 })
 export class HireMePage implements OnInit {
 
