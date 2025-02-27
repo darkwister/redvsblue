e4ad7dd4bf70c1f38c2ee375490ca27b26fa78e6
DarkWister <darkwistercrak17@gmail.com>
2025-02-26 21:36:25 -0400

    Moments handler done

--- src/app/components/moments/moments.component.ts
+++ src/app/components/moments/moments.component.ts
@@ -1,17 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {IonItem, IonList, IonGrid, IonCol, IonRow,} from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { SwiperOptions, Swiper } from 'swiper/types';
 
 
 @Component({
   selector: 'app-moments',
   templateUrl: './moments.component.html',
   styleUrls: ['./moments.component.scss'],
-  imports: [ CommonModule, IonItem, IonList, IonGrid, IonCol, IonRow,],
+  imports: [ CommonModule, IonicModule],
   standalone: true
 })
 export class MomentsComponent  implements OnInit {
+
+  slidesOptions: SwiperOptions = {
+    initialSlide: 0,
+    direction: 'horizontal',
+    speed: 300,
+    effect: 'slide',
+    spaceBetween: 8,
+    slidesPerView: 1.5,
+    freeMode: true,
+    loop: true
+  };
 
   @Input() moments!: momentsList[];
   safeUrls: SafeResourceUrl[] = [];
