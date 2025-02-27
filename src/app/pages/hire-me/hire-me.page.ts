4ef73bf3f433eca47dd77e0ef68a42573e97f24e
DarkWister <darkwistercrak17@gmail.com>
2025-02-27 19:47:03 -0400

    hire me module finished

--- src/app/pages/hire-me/hire-me.page.ts
+++ src/app/pages/hire-me/hire-me.page.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg, IonIcon, IonRow} from '@ionic/angular/standalone';
+import { logoGithub, logoLinkedin } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-hire-me',
   templateUrl: './hire-me.page.html',
   styleUrls: ['./hire-me.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,  IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,  IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonList, IonItem, IonLabel, IonAvatar, IonImg, IonIcon, IonRow]
 })
 export class HireMePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({logoGithub, logoLinkedin});
+  }
 
   ngOnInit() {
   }
