ad22c1d71500452135dd0a57f9be56bde5ebf287
DarkWister <darkwistercrak17@gmail.com>
2025-02-27 17:06:29 -0400

    about serie module finished

--- src/app/pages/about-serie/about-serie.page.ts
+++ src/app/pages/about-serie/about-serie.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-about-serie',
   templateUrl: './about-serie.page.html',
   styleUrls: ['./about-serie.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar]
 })
 export class AboutSeriePage implements OnInit {
 
