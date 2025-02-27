c9627c8c6554df3050c20ed61ce8d3c03ee357e9
DarkWister <darkwistercrak17@gmail.com>
2025-02-27 19:23:12 -0400

    about serie modularized and added the characters implementation

--- src/app/pages/about-serie/about-serie.page.ts
+++ src/app/pages/about-serie/about-serie.page.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg} from '@ionic/angular/standalone';
+import { personajes } from '../characters/characters.page';
 
 @Component({
   selector: 'app-about-serie',
   templateUrl: './about-serie.page.html',
   styleUrls: ['./about-serie.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar]
+  imports: [ IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonText, IonList, IonListHeader, IonItem, IonLabel, IonAvatar, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonImg]
 })
 export class AboutSeriePage implements OnInit {
-
+  personajes = personajes;
   constructor() { 
   }
 
